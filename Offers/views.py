29840ff419d6527d8874cfcd8a36287fb1b3a4ea
rciup <rciupek0@gmail.com>
2022-04-26 23:22:26 +0200

    Login and Register plus some first sites

--- Offers/views.py
+++ Offers/views.py
@@ -1,7 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login
 from django.http import HttpResponse
 
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. You're at the offers site.")
+    return render(request, "Offers/base.html", {})
+
+
+def home(request):
+    return render(request, "Offers/home.html", {})
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return redirect('index')
+    else:
+        form = UserCreationForm()
+
+    context = {'form' : form }
+    return render(request, 'registration/register.html', context)
