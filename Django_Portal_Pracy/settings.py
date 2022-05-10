641ec6b898848d544b8ac29ef9ae10283f7549bc
rciup <rciupek0@gmail.com>
2022-05-10 17:33:45 +0200

    Revert "App name changes from Offres to Site"
    This reverts commit be6f09c4

--- Django_Portal_Pracy/settings.py
+++ Django_Portal_Pracy/settings.py
@@ -30,7 +30,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'Site.apps.OffersConfig',
+    'Offers.apps.OffersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
